c9451c0cbae52dc30c87ad5f42b05b1726697eb1
Connor Spencer Harries <connorsharries96@gmail.com>
2014-04-13 03:57:37 +0100

    A lot of debugging and just fiddling

--- src/main/java/io/github/charries96/pvpmoney/Notifier.java
+++ src/main/java/io/github/charries96/pvpmoney/Notifier.java
@@ -27,7 +27,7 @@ public final class Notifier extends JavaPlugin implements Listener {
     private String deathmsg = "&4You were killed by %killer%!";
     private String value = "10";
     private String extra = "15";
-    private char currency = '£';
+    private String currency = "£";
     private Boolean debug = false;
 	
 	@Override
@@ -49,52 +49,47 @@ public final class Notifier extends JavaPlugin implements Listener {
 			getLogger().info("Permissions found, using " + permissions.getName() + ".");
 		
 		loadConfig();
+		getLogger().info("Currency symbol: " + currency);
+		getLogger().info("Reward (w/o Extra): " + value);
+		getLogger().info("Reward (w/ Extra): " + extra);
 		getLogger().info("Loaded config.yml values");
 	}
 	
 	private void loadConfig() {
+		
 		if(this.getConfig().getString("pvpmoney.prefix") != "" && this.getConfig().getString("pvpmoney.prefix").length() >= 1)
-			prefix = this.getConfig().getString("pvpmoney.prefix") + " ";
+			prefix = this.getConfig().getString("pvpmoney.prefix");
+		
 		if(this.getConfig().getString("pvpmoney.messages.killer") != "" && this.getConfig().getString("pvpmoney.messages.killer").length() >= 1)
 			killmsg = this.getConfig().getString("pvpmoney.messages.killer");
+		
 		if(this.getConfig().getString("pvpmoney.messages.victim") != "" && this.getConfig().getString("pvpmoney.messages.victim").length() >= 1)
 			deathmsg = this.getConfig().getString("pvpmoney.messages.victim");
+		
 		if(this.getConfig().getString("pvpmoney.rewards.amount") != "" && this.getConfig().getString("pvpmoney.rewards.amount").length() >= 1)
 			value = this.getConfig().getString("pvpmoney.rewards.amount");
+		
 		if(this.getConfig().getString("pvpmoney.rewards.currency") != "" && this.getConfig().getString("pvpmoney.rewards.currency").length() >= 1)
-			currency = this.getConfig().get("pvpmoney.rewards.currency").toString().charAt(1);
+			currency = this.getConfig().getString("pvpmoney.rewards.currency");
+		
 		if(this.getConfig().getString("pvpmoney.rewards.ranked") != "" && this.getConfig().getString("pvpmoney.rewards.ranked").length() >= 1)
 			extra = this.getConfig().getString("pvpmoney.rewards.ranked");
+		
 		debug = this.getConfig().getBoolean("pvpmoney.debug");
-	}
-	
-	private void saveConfiguration() {
-		this.getConfig().set("pvpmoney.prefix", prefix);
-		this.getConfig().set("pvpmoney.rewards.amount", Double.parseDouble(value));
-		this.getConfig().set("pvpmoney.rewards.ranked", Double.parseDouble(extra));
-		this.saveConfig();
 	}
 
 	@Override
 	public void onDisable() {
-		getConfig().set("pvpmoney.prefix", prefix);
-		getConfig().set("pvpmoney.rewards.amount", value);
-		getConfig().set("pvpmoney.rewards.ranked", extra);
-		getConfig().set("pvpmoney.rewards.currency", extra);
-		getConfig().set("pvpmoney.messages.killer", killmsg);
-		getConfig().set("pvpmoney.messages.victim", deathmsg);
-		saveConfig();
+		this.saveConfig();
 		getLogger().info("Saved configuration file.");
 	}
 			
 	private void doHelp(CommandSender sender) {
 		sender.sendMessage(ChatColor.AQUA + "------ " + ChatColor.DARK_AQUA + "[" + ChatColor.GOLD + "PvPMoney by charries96" + ChatColor.DARK_AQUA + "]" + ChatColor.AQUA + " ------");
 
-		sendHelp(sender, "extra", "<amount>", "Set money per kill for those with pvpmoney.extra");
 		sendHelp(sender, "help", "Display this page");
-		sendHelp(sender, "money", "<amount>", "Set money per kill value");
-		sendHelp(sender, "reload", "Reload values from config.yml");
-		sendHelp(sender, "save", "Save config.yml");
+		sendHelp(sender, "extra", "<amount>", "Set money per kill (w/ pvpmoney.extra)");
+		sendHelp(sender, "money", "<amount>", "Set money per kill (w/o pvpmoney.extra)");
 		sendHelp(sender, "test", "Display messages users would see");
 	}
 	
@@ -104,57 +99,45 @@ public final class Notifier extends JavaPlugin implements Listener {
 			if(args.length < 1 || args == null)
 				doHelp(sender);
 			if(args != null && args.length > 0) {
-				if(args[0] != null && args[0].length() >= 1) {
-					if(args[0] == "extra") {
-						if(args[1] != null && args[1].length() >= 1) {
-							try {
-								double nmoney = Double.parseDouble(args[1]);
-								extra = "" + nmoney;
-								sender.sendMessage(ChatColor.GREEN + replaceValue(replaceCurrency("Players will now receive %currency%%reward% per kill."), true));
-								return true;
-							} catch (NumberFormatException e) {
-								sender.sendMessage(ChatColor.RED + "Error parsing value.");
-							}
-						} else {
-							sender.sendMessage(ChatColor.GREEN + "Value: " + extra);
-							sender.sendMessage(ChatColor.GREEN + "To set: /pvpmoney extra <amount>");
+				if(args[0].equalsIgnoreCase("extra")) {
+					if(args[1] != null && args[1].length() >= 1) {
+						try {
+							double nmoney = Double.parseDouble(args[1]);
+							extra = "" + nmoney;
+							sender.sendMessage(ChatColor.GREEN + replaceValue(replaceCurrency("Players will now receive %currency%%reward% per kill."), true));
+							this.getConfig().set("pvpmoney.rewards.ranked", Double.parseDouble(extra));
+							this.saveConfig();
+							return true;
+						} catch (NumberFormatException e) {
+							sender.sendMessage(ChatColor.RED + "Error parsing value.");
+							return false;
 						}
-						return false;
+					} else {
+						sender.sendMessage(ChatColor.GREEN + "Value: " + extra);
+						sender.sendMessage(ChatColor.GREEN + "To set: /pvpmoney extra <amount>");
 					}
-					if(args[0] == "money") {
-						if(args[1] != null && args[1].length() >= 1) {
-							try {
-								double nmoney = Double.parseDouble(args[1]);
-								value = "" + nmoney;
-								sender.sendMessage(ChatColor.GREEN + replaceValue(replaceCurrency("Players will now receive %currency%%reward% per kill."), false));
-								return true;
-							} catch (NumberFormatException e) {
-								sender.sendMessage(ChatColor.RED + "Error parsing value.");
-							}
-						} else {
-							sender.sendMessage(ChatColor.GREEN + "Value: " + value);
-							sender.sendMessage(ChatColor.GREEN + "To set: /pvpmoney money <amount>");
+					return false;
+				} else if(args[0].equalsIgnoreCase("money")) {
+					if(args[1] != null && args[1].length() >= 1) {
+						try {
+							double nmoney = Double.parseDouble(args[1]);
+							value = "" + nmoney;
+							sender.sendMessage(ChatColor.GREEN + replaceValue(replaceCurrency("Players will now receive %currency%%reward% per kill."), false));
+							this.getConfig().set("pvpmoney.rewards.amount", Double.parseDouble(value));
+							this.saveConfig();
+							return true;
+						} catch (NumberFormatException e) {
+							sender.sendMessage(ChatColor.RED + "Error parsing value.");
+							return false;
 						}
-						return false;
+					} else {
+						sender.sendMessage(ChatColor.GREEN + "Value: " + value);
+						sender.sendMessage(ChatColor.GREEN + "To set: /pvpmoney money <amount>");
 					}
-					
-					if(args[0] == "save") {
-						saveConfiguration();
-						sender.sendMessage(colourise(prefix) + ChatColor.GREEN + "Configuration saved.");
-						return true;
-					}
-					if(args[0] == "reload") {
-						this.reloadConfig();
-						loadConfig();
-						sender.sendMessage(ChatColor.GREEN + "Reloaded config.yml");
-						return true;
-					}
-					if(args[0] == "test") {
-						spoof(sender, "Dinnerbone", "God");
-						return true;
-					}
-				}
-				else {
+				} else if(args[0].equalsIgnoreCase("test")) {
+					spoof(sender, "Dinnerbone", "Dinnerbone");
+					return true;
+				} else {
 					doHelp(sender);
 					return false;
 				}
@@ -164,9 +147,9 @@ public final class Notifier extends JavaPlugin implements Listener {
 	}
 	
 	public void spoof(CommandSender sender, String killer, String victim) {
-		sender.sendMessage(replaceValue(replaceCurrency(replaceVictim(replaceKiller(killmsg, killer), victim)), false));
-		sender.sendMessage(replaceValue(replaceCurrency(replaceVictim(replaceKiller(killmsg, killer), victim)), true));
-		sender.sendMessage(replaceKiller(deathmsg, killer));
+		sender.sendMessage(colourise(replaceValue(replaceCurrency(replaceVictim(replaceKiller(killmsg, killer), victim)), false)));
+		sender.sendMessage(colourise(replaceValue(replaceCurrency(replaceVictim(replaceKiller(killmsg, killer), victim)), true)));
+		sender.sendMessage(colourise(replaceKiller(deathmsg, killer)));
 		sender.sendMessage(ChatColor.RED + replaceCurrency("Currency Symbol: %currency%"));
 		sender.sendMessage(ChatColor.RED + replaceValue("Regular Reward: %reward%", false));
 		sender.sendMessage(ChatColor.RED + replaceValue("\"Extra\" Reward: %reward%", true));
@@ -193,7 +176,7 @@ public final class Notifier extends JavaPlugin implements Listener {
 			// END: Get Killer & Victim name
 			
 			// Tell our Victim about their death.
-			victim.sendMessage(colourise(prefix + " " + colourise(replaceKiller(deathmsg, killern))));
+			victim.sendMessage(colourise(prefix + " " + replaceKiller(deathmsg, killern)));
 						
 			EconomyResponse r = null;
 			if(permissions.has(killer, "pvpmoney.extra"))
@@ -204,16 +187,16 @@ public final class Notifier extends JavaPlugin implements Listener {
 			if(r.transactionSuccess()) {
 				// Make the Killers day
 				if(permissions.has(killer, ""))
-					killer.sendMessage(replaceCurrency(colourise(prefix) + replaceValue(replaceVictim(killmsg, victimn), true)));
+					killer.sendMessage(replaceCurrency(colourise(prefix) + " " + replaceValue(replaceVictim(killmsg, victimn), true)));
 				else
-					killer.sendMessage(replaceCurrency(colourise(prefix) + replaceValue(replaceVictim(killmsg, victimn), false)));
+					killer.sendMessage(replaceCurrency(colourise(prefix) + " " + replaceValue(replaceVictim(killmsg, victimn), false)));
             } else {
             	// Oh no, better compensate them!
-            	killer.sendMessage(prefix + ChatColor.DARK_RED + "An error occured when rewarding you.");
+            	killer.sendMessage(prefix + ChatColor.DARK_RED + " An error occured when rewarding you.");
             	
             	// Blame the Owner
             	if((economy == null))
-            		killer.sendMessage(prefix + ChatColor.RED + "Ask your server administrator to install a Vault compatible economy.");
+            		killer.sendMessage(prefix + ChatColor.RED + " Ask your server administrator to install a Vault compatible economy.");
             	
             	// Give them a diamond to compensate
             	killer.getInventory().addItem(new ItemStack(Material.DIAMOND, 1));
@@ -253,7 +236,7 @@ public final class Notifier extends JavaPlugin implements Listener {
 	
 	public String replaceCurrency(String str) {
 		if(str.contains("%currency%"))
-			return str.replace("%currency%", ("" + currency));
+			return str.replace("%currency%", currency);
 		return str;
 	}
 	
